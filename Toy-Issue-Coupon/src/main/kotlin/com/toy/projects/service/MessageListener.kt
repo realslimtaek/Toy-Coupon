fb5a201dcfdd2ab0b7ef5e02f7f505ba0f81b21f
realslimtaek <xorgud777@gmail.com>
2026-03-08 14:52:14 +0900

    refactor: 큐 메세지 리스너 수정
    queue 상수로 적용
    동기처리하는 코드를 비동기 처리로 수정

--- Toy-Issue-Coupon/src/main/kotlin/com/toy/projects/service/MessageListener.kt
+++ Toy-Issue-Coupon/src/main/kotlin/com/toy/projects/service/MessageListener.kt
@@ -1,5 +1,6 @@
 package com.toy.projects.service
 
+import com.toy.projects.config.RabbitConfig
 import com.toy.projects.service.mq.dto.IssueCouponMessageDto
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.stereotype.Service
@@ -11,18 +12,17 @@ class MessageConsumer(
     private val couponService: CouponService
 ) {
 
-    @RabbitListener(queues = ["my.spring4.queue"])
+    @RabbitListener(queues = [RabbitConfig.QUEUE_NAME])
     fun receiveMessages(dataList: List<IssueCouponMessageDto>) {
         println("<<< 메시지 배치 수신: ${dataList.size}개")
-        
-        // 10개를 비동기적으로 동시에 처리
         Flux.fromIterable(dataList)
             .flatMap({ data ->
-                couponService.handleMessage(data)
-            }, 10) // 병렬 처리 개수 설정
-            .collectList()
-            .block() // 배치가 완료될 때까지 대기하여 Ack를 보냄
-            
-        println("<<< 메시지 배치 처리 완료")
+                couponService.handleMessage(data).retry(3)
+            }, 10)
+            .then()
+            .subscribe(
+                { println("<<< 메시지 배치 처리 완료") },
+                { e -> println("<<< 메시지 배치 처리 실패: ${e.message}") }
+            )
     }
 }
