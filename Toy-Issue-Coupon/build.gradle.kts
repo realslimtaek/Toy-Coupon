b25ea3eb7ea72ce816406abd420f8e9b14156a80
realslimtaek <xorgud777@gmail.com>
2026-03-08 14:49:58 +0900

    refactor : 중복되는 의존성 제거

--- Toy-Issue-Coupon/build.gradle.kts
+++ Toy-Issue-Coupon/build.gradle.kts
@@ -26,7 +26,6 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-r2dbc")
     implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("org.springframework.boot:spring-boot-starter-webmvc")
     implementation("io.projectreactor.kotlin:reactor-kotlin-extensions")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
